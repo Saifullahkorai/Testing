93f854d6be02de4e4fe1d5aa3749f2196ac6f92f
Saifullah Khan <saifullahkhankorai60@gmail.com>
2022-01-30 21:30:48 +0500

    Variables in c

---
+++ variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a=5, b=6;
+    float i=5;
+    char c='A';
+    printf("The sum of a+b %d %d\n", a,a);
+    printf("this is float value %f %f\n", i,i);
+    printf("this is character %c %c\n", c,c);
+    return 0;
+}
